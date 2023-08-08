dc289ffa59058a81bf568149e34f1c24ef8d5650
sameershaikkcdc <134685421+sameershaikkcdc@users.noreply.github.com>
2023-08-08 18:07:42 +0530

    Update Shaik_Raw_error.sql

--- Shaik_Raw/models/Shaik_Raw_error.sql
+++ Shaik_Raw/models/Shaik_Raw_error.sql
@@ -1,3 +1,11 @@
+{{ config(
+    materialized='incremental',
+    partition_by={
+          "field": "processed_ts",
+          "data_type": "timestamp",
+          "granularity": "day"
+        }
+) }}	
 select 
 	Name,
 	course,
@@ -5,3 +13,6 @@ select
 	year
 fron {{ ref('Shaik_Raw_temp') }} temp
 where array_length(temp.failure_reason) > 0
+{% if is_incremental() %}
+    and temp.processed_ts > ( SELECT COALESCE(max(processed_ts), TIMESTAMP('2000-01-01 00:00:00.0000')) from {{ this }} )
+{% endif %}
